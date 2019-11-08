439b4a5bef7470c0ae3fa706e72cea791f933c20
Larisa Leucuta <larisa.leucuta@msg.group>
2019-11-08 12:29:21 +0200

    First and second chapter

---
+++ online-shop/src/app/mock-products.ts
@@ -0,0 +1,8 @@
+import { Product } from './product'
+
+export const PRODUCTS: Product[]=[
+    { id: 1, name: 'Phone' ,price: 2000},
+    { id: 2, name: 'TV' ,price: 3500},
+    { id: 3, name: 'Laptop' ,price: 2300},
+    { id: 4, name: 'Fridge' ,price: 3000},
+]
---
+++ online-shop/src/app/products/products.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { PRODUCTS } from '../mock-products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products= PRODUCTS;
+  selectedProduct: Product;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(product:Product):void{
+    this.selectedProduct=product;
+  }
+
+}
